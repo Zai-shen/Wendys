6353845b7fc69739e06cdfb0d5383b4517dcae6e
MJlapone\miran <e1526438@mail.student.tuwien.ac.at>
2020-03-17 17:21:46 +0100

    Modified horse component to work analogously to owner component. US1

--- frontend/wendys-friends/src/app/component/horse/horse.component.ts
+++ frontend/wendys-friends/src/app/component/horse/horse.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Horse} from "../../dto/horse";
+import {HorseService} from "../../service/horse.service";
+
 @Component({
   selector: 'app-horse',
   templateUrl: './horse.component.html',
   styleUrls: ['./horse.component.scss']
 })
+
 export class HorseComponent implements OnInit {
 
-  constructor() { }
+  error = false;
+  errorMessage = '';
+  horse: Horse;
 
-  ngOnInit(): void {
+  constructor(private horseService: HorseService) {
+  }
+
+  ngOnInit() {
+    this.loadHorse(1);
+  }
+
+  /**
+   * Error flag will be deactivated, which clears the error message
+   */
+  vanishError() {
+    this.error = false;
+  }
+
+  /**
+   * Loads the owner for the specified id
+   * @param id the id of the owner
+   */
+  private loadHorse(id: number) {
+    this.horseService.getHorseById(id).subscribe(
+      (horse: Horse) => {
+        this.horse = horse;
+      },
+      error => {
+        this.defaultServiceErrorHandling(error);
+      }
+    );
+  }
+
+  private defaultServiceErrorHandling(error: any) {
+    console.log(error);
+    this.error = true;
+    if (error.status === 0) {
+      // If status is 0, the backend is probably down
+      this.errorMessage = 'The backend seems not to be reachable';
+    } else if (error.error.message === 'No message available') {
+      // If no detailed error message is provided, fall back to the simple error name
+      this.errorMessage = error.error.error;
+    } else {
+      this.errorMessage = error.error.message;
+    }
   }
 
 }
+
